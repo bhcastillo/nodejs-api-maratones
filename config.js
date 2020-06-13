ea3c043c29af10744f4a0388b49350d2bab7af4e
Brayan Castillo <bhcastillo@uniboyaca.edu.co>
2020-06-12 23:21:09 -0500

    make it better

--- config.js
+++ config.js
@@ -3,4 +3,6 @@ process.env.PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === 'dev') {
   process.env.URL_DB = 'mongodb://127.0.0.1:27017/maratones';
+} else {
+  process.env.URL_DB = '';
 }
