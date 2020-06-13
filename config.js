d0199d0020c5c0788e84576987f7a66060a2876a
Brayan Castillo <bhcastillo@uniboyaca.edu.co>
2020-06-12 23:34:09 -0500

    dev

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ process.env.PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV === 'dev') {
   process.env.URL_DB = 'mongodb://127.0.0.1:27017/maratones';
 } else {
-  process.env.URL_DB = '';
+  process.env.URL_DB =
+    'mongodb+srv://bhcastillo:edteam@cluster0-qlnk6.mongodb.net/bcmaratones?retryWrites=true&w=majority';
 }
